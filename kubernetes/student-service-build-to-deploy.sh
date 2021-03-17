72cf6c977c49bf5845f54aac8c25e38961fc7828
Amlan Datta <>
2021-03-17 11:21:55 +0530

    deploy student service to kubernetes

--- kubernetes/student-service-build-to-deploy.sh
+++ kubernetes/student-service-build-to-deploy.sh
@@ -13,6 +13,14 @@ echo "load image to kind cluster"
 
 kind load docker-image ad-library/spring310-student-service:0.0.1-SNAPSHOT --name spring-world
 
+echo "***********************************"
+echo "creating configMap spring-with-k8s-config"
+
+#kubectl create configmap spring-with-k8s-config \
+#    --from-file=${HOME}/workspace/config-repo/course-service.properties \
+#    --from-file=${HOME}/workspace/config-repo/student-service.properties \
+#    -n spring-world-ns
+
 cd ~/workspace/world-of-spring/vmw/spring310/kubernetes/spring-with-k8s/
 
 echo "***********************************"
